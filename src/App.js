9bec5c399a2856ee6935d36eac922e25e8d33fd5
MajidZanjani <majidmmz1972@gmail.com>
2023-12-07 22:00:38 +0400

    eBookStore

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Header, Footer } from './components';
+import { AllRoutes } from './routes/AllRoutes';
+
+function App() {
+  return (
+    <div className="App dark:bg-dark">
+      <Header />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
