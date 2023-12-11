93639e8b45ee47f987b00899874dc3feb9f9f5f2
MajidZanjani <majidmmz1972@gmail.com>
2023-12-11 23:00:46 +0400

    render add

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,6 +4,7 @@ export async function login(authDetail) {
     headers: { 'content-Type': 'application/json' },
     body: JSON.stringify(authDetail),
   };
+
   const response = await fetch(`${process.env.REACT_APP_HOST}/login`, requestOptions);
   if (!response.ok) {
     throw { message: response.statusText, status: response.status } //eslint-disable-line
